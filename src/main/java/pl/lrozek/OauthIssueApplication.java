76615ba469c4ed0554d3dd242d5bf97a19a36fc1
Lukasz Rozek <lukasz.rozek84@gmail.com>
2016-12-22 15:32:45 +0100

    initial

---
+++ src/main/java/pl/lrozek/OauthIssueApplication.java
@@ -0,0 +1,12 @@
+package pl.lrozek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthIssueApplication.class, args);
+	}
+}
