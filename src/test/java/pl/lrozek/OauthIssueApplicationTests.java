772bcf5d88b0b558b46917c7dd6578af724fc7a7
Lukasz Rozek <lukasz.rozek84@gmail.com>
2016-12-22 15:41:21 +0100

    mockmvc injection

--- src/test/java/pl/lrozek/OauthIssueApplicationTests.java
+++ src/test/java/pl/lrozek/OauthIssueApplicationTests.java
@@ -2,15 +2,26 @@ package pl.lrozek;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest
 public class OauthIssueApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    public void contextLoads() {
+    public void itShould() throws Exception {
+        //given
+
+        //when
+
+        //then
+
     }
 
 }
