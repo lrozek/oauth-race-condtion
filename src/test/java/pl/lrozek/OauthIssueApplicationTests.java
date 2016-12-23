a11d7ac552833a9168ac027b066ed0354ffb37f1
Lukasz Rozek <lukasz.rozek84@gmail.com>
2016-12-23 12:16:35 +0100

    multithreaded

--- src/test/java/pl/lrozek/OauthIssueApplicationTests.java
+++ src/test/java/pl/lrozek/OauthIssueApplicationTests.java
@@ -37,7 +37,7 @@ public class OauthIssueApplicationTests extends AbstractTestNGSpringContextTests
         return new Object[][] { { "Bradley" }, { "Aldis" } };
     }
 
-    @Test(dataProvider = "users")
+    @Test(dataProvider = "users", threadPoolSize = 2, invocationCount = 2)
     public void itShouldReturnUserName( String token ) throws Exception {
         //given
 
